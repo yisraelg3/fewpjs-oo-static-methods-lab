c987e29e76c16ecc0db3155759137a435d6ca76c
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-09 14:10:26 -0400

    commit tree e0d14592fe4c3cd31d0b4e23bab89fa8cb105d7b

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
   //add static methods here
+  static capitalize (string) {
+    return ( string[0].toUpperCase() + string.slice(1))
+  }
+
+  static sanitize (string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize (string) {
+    const excludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    return string.split(' ').map((word, i) => {
+      return (excludeWords.includes(word) && i ? word : this.capitalize(word))
+    }).join(' ')
+  }
 }
+ 
